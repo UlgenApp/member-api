02dedf4ea3bffbef75fc6e82dcbe07bc78c1d0cf
Kaan Türkmen <kaanturkmen@protonmail.com>
2023-05-09 12:40:46 +0300

    Added fallback signature.

--- src/main/java/tr/edu/ku/ulgen/client/fallback/FeignClientFallback.java
+++ src/main/java/tr/edu/ku/ulgen/client/fallback/FeignClientFallback.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import tr.edu.ku.ulgen.client.ProducerClient;
 import tr.edu.ku.ulgen.client.RouterClient;
+import tr.edu.ku.ulgen.dto.HeatmapDataDto;
 import tr.edu.ku.ulgen.dto.ProducerDto;
 import tr.edu.ku.ulgen.dto.RouteDataDto;
 
@@ -37,6 +38,17 @@ public class FeignClientFallback implements ProducerClient, RouterClient {
      */
     @Override
     public ResponseEntity<JsonNode> route(RouteDataDto routeDataDto) {
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    /**
+     * Fallback method for the router client.
+     *
+     * @param heatmapDataDto the heatmap data transfer object containing the required route information,
+     * @return a {@link ResponseEntity} with a {@link HttpStatus#SERVICE_UNAVAILABLE} status.
+     */
+    @Override
+    public ResponseEntity<JsonNode> heatmap(HeatmapDataDto heatmapDataDto) {
         return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
